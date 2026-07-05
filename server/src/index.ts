e2ddaae287ce45ba88e079ce0cbc5e8a64044bc8
Tejas Gupta <tejasporwal111@gmail.com>
2026-07-06 01:14:00 +0530

    Updated Report.md

--- server/src/index.ts
+++ server/src/index.ts
@@ -49,7 +49,7 @@ function broadcast(event: SSEEvent) {
 
 app.get("/report", (_req, _res) => {
   try {
-    const reportFilePath = path.resolve(process.cwd(), "./Report.md");
+    const reportFilePath = path.resolve(process.cwd(), "./report.md");
 
     const markdownContent = fs.readFileSync(reportFilePath, "utf-8");
 
